416a7b390eae9c0736ee1a0572cfb2bb66db67b3
Andrefer25 <andrefer2505@gmail.com>
2021-07-12 00:24:08 -0300

    feat: adding home sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
