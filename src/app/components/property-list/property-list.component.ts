9cdd4ed4d32df73e0f6f08a6b07ec94006923771
Sofia Espada <sofiaespada20@gmail.com>
2021-07-19 18:57:18 -0300

    rebase with main

--- src/app/components/property-list/property-list.component.ts
+++ src/app/components/property-list/property-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.scss']
 })
 export class PropertyListComponent implements OnInit {
+  @Input() propiedadesExtranjeras: boolean = true;
+  
 
   constructor() { }
+  
 
   ngOnInit(): void {
   }
