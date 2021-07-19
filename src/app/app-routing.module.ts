e8619c2bb87db9d9c90d97698bcffdb39e5041de
Sofia Espada <sofiaespada20@gmail.com>
2021-07-19 19:51:55 -0300

    add propiedad routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { OportunidadesComponent } from './components/oportunidades/oportunidades.component';
 import { PropertyListComponent } from "./components/property-list/property-list.component";
+import { PropiedadComponent } from "./components/propiedad/propiedad.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'oportunidades', component: OportunidadesComponent},
+  { path: 'oportunidades', component: OportunidadesComponent },
+  { path: 'propiedad', component:PropiedadComponent },
   { path: 'propiedades', redirectTo: 'propiedades/', pathMatch: 'full'},
   { path: 'propiedades/:extranjeras', component: PropertyListComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
